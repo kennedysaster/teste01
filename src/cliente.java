798f2b0a82c6d0f280fc623b8d168ccb34c397a7
kennedysaster <kennedysaster@gmail.com>
2023-01-17 12:44:19 -0300

    Atualizacao

--- src/cliente.java
+++ src/cliente.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class cliente {
     public static void main(String[] args) {
         System.out.println("Bem vindo ao Intllij");
-
+        System.out.println("Bem vindo ao Intllij");
+        System.out.println("Bem vindo ao Intllij");
+        System.out.println("Marcos Kenndy");
+        return "Marcos";
     }
+
 }
